afc5aa678595cfd245b65465bc6659899726c327
loup.mesquita <loup.mesquita@epitech.eu>
2021-05-07 18:21:35 +0200

    decrypt + errors + classes

--- RubyCaesar.rb
+++ RubyCaesar.rb
@@ -6,7 +6,35 @@
 ## Caesar
 ##
 
-def encrypt( text)
+#classes
+
+class Error
+
+    def initialize(string)
+        @string = string
+    
+    end
+
+    def is_number?
+        true if Float(@string) rescue false
+    end
+    def validate
+        chars = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
+        @string.chars.detect {|ch| !chars.include?(ch)}.nil?
+    end
+
+end 
+
+class Caesar
+
+#initialize class
+  def initialize(shift)
+    @shift = shift
+
+  end
+
+##encrypt part
+  def encrypt( text)
     
     result = ""
 ## transform char into corresponding ascii int 
@@ -32,3 +60,32 @@ def encrypt( text)
     end
     return result
   end
+# decrypt part which is the inverse of encrypt  
+  def decrypt( encrypt)
+    @shift = @shift * -1
+    encrypt( encrypt)
+  end
+  
+end
+
+text = ARGV[0]
+
+errorStr = Error.new(ARGV[0])
+errorNb = Error.new(ARGV[1])
+#checking errors
+if ((!errorNb.is_number?) || (ARGV[1].to_i > 1) || ((ARGV[1].to_i).negative?) || !errorStr.validate)
+    exit (84)
+end
+
+#print result
+
+puts "text: #{text}"
+
+
+caesar = Caesar.new(21)
+cipherText = caesar.encrypt( text)
+if (ARGV[1] == "0")
+puts "Ciphered text: #{cipherText}"
+elsif (ARGV[1] == "1")
+puts "Deciphered text: #{caesar.decrypt( text)}"
+end
